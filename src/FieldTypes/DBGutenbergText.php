ebacd4717cb834e17c61d9723be579a482303a60
Lee Bradley <lee.bradley@mademedia.co.uk>
2018-04-26 15:45:36 +0100

    Add some comments, remove un-used/commented code
    Also add extra check to prevent errors if invalid block is
      passed in

--- src/FieldTypes/DBGutenbergText.php
+++ src/FieldTypes/DBGutenbergText.php
@@ -29,13 +29,10 @@ class DBGutenbergText extends DBText
 
     public function forTemplate()
     {
-        $this->value = '<!-- wp:paragraph {"align":"right"} -->\n<p>Here is a test of the gutenberg plugin<\/p>\n<!-- \/wp:paragraph -->\n\n<!-- wp:paragraph -->\n<p>A second paragraph to use<\/p>\n<!-- \/wp:paragraph -->';
         $partDetails = $this->getPartDetails();
         $blockProcessors = static::config()->uninherited('blockProcessors');
 
         $html = array_reduce($partDetails, function ($carry, $block) use ($blockProcessors) {
-            // $partProcessor = sprintf('partProcessor%s%s', ucwords($part['namespace']), ucwords($part['block']));
-            // $carry.= $this->$partProcessor($part);
             $classKey = sprintf('%s:%s', $block['namespace'], $block['block']);
             if (array_key_exists($classKey, $blockProcessors)) {
                 $class = $blockProcessors[$classKey];
@@ -46,24 +43,30 @@ class DBGutenbergText extends DBText
             return $carry;
         }, '');
         return $html;
-        var_dump($partDetails, $html);
-        die;
     }
 
+    /**
+     * Splits the commented RAW data value into the individual block elements
+     *   then processes each part to get an array with `namespace`, `block`,
+     *   `attributes` and `data` keys
+     * @return Array
+     */
     protected function getPartDetails() {
         $rawParts = preg_split('(<!--\s+\\\/(\w+):(\w+)\s+-->)', $this->value);
         $partDetails = array_reduce($rawParts, function ($carry, $item) {
             if (trim($item)) {
                 preg_match('/<!--\s+(\w+):(\w+)\s+(\{.*\})?\s?-->(.*)/', $item, $matches);
-                array_push($carry, [
-                    'namespace' => $matches[1],
-                    'block' => $matches[2],
-                    'attributes' => json_decode($matches[3], true),
-                    'data' => strtr($matches[4], [
-                        '\/' => '/',
-                        '\n' => "\n",
-                    ]),
-                ]);
+                if ($matches) {
+                    array_push($carry, [
+                        'namespace' => $matches[1],
+                        'block' => $matches[2],
+                        'attributes' => json_decode($matches[3], true),
+                        'data' => strtr($matches[4], [
+                            '\/' => '/',
+                            '\n' => "\n",
+                        ]),
+                    ]);
+                }
             }
             return $carry;
         }, []);
