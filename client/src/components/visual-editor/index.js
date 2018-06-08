af262eb3c8df6f3f0f0d14979d126da95b61a56c
hirenpatel <hiren.patel@mademedia.co.uk>
2018-06-08 15:50:44 +0100

    Pushing some minor style amends

--- client/src/components/visual-editor/index.js
+++ client/src/components/visual-editor/index.js
@@ -20,25 +20,26 @@ import { withViewportMatch } from '@wordpress/viewport';
 import './style.scss';
 import BlockInspectorButton from './block-inspector-button';
 
+function VisualEditorBlockMenu( { children, onClose } ) {
+    return (
+        <Fragment>
+            <BlockInspectorButton onClick={ onClose } role="menuitem" />
+            { children }
+        </Fragment>
+    );
+}
+
 function VisualEditor( { hasFixedToolbar, isLargeViewport } ) {
     return (
         <BlockSelectionClearer className="edit-post-visual-editor">
             <EditorGlobalKeyboardShortcuts />
             <CopyHandler />
             <MultiSelectScrollIntoView />
-            <ObserveTyping>
-                <WritingFlow>
-                    <BlockList
-                        showContextualToolbar={ ! isLargeViewport || ! hasFixedToolbar }
-                        renderBlockMenu={ ( { children, onClose } ) => (
-                            <Fragment>
-                                <BlockInspectorButton onClick={ onClose } />
-                                { children }
-                            </Fragment>
-                        ) }
-                    />
-                </WritingFlow>
-            </ObserveTyping>
+            <WritingFlow>
+                <ObserveTyping>
+                    <BlockList renderBlockMenu={ VisualEditorBlockMenu } />
+                </ObserveTyping>
+            </WritingFlow>
         </BlockSelectionClearer>
     );
 }
