a08acd81426bebf3a6e10e40d5da05e348422ed1
Lee Bradley <lee.bradley@mademedia.co.uk>
2018-07-12 14:39:59 +0100

    Fix render for Image2Block, was just using $content

--- src/Blocks/Image2Block.php
+++ src/Blocks/Image2Block.php
@@ -2,6 +2,12 @@
 
 namespace MadeHQ\Gutenberg\Blocks;
 
+use SilverStripe\Assets\Image;
+use SilverStripe\Assets\File;
+
+use SilverStripe\ORM\ArrayList;
+use SilverStripe\View\ArrayData;
+
 class Image2Block extends BaseBlock
 {
     /**
@@ -30,6 +36,26 @@ class Image2Block extends BaseBlock
 
     public function render($content, array $attributes = array())
     {
-        return $content;
+        $template = $this->getTemplateData($attributes);
+        return $template->renderWith('InlineGallery');
+    }
+
+    private function getTemplateData($attributes)
+    {
+        $images = array_reduce($attributes['images'], function ($carry, $imageData) {
+            $image = File::get_by_id(File::class, $imageData['id']);
+            if (is_object($image) && $image->exists()) {
+                $carry->push(ArrayData::create([
+                    'Image' => $image,
+                    'Caption' => array_key_exists('caption', $imageData) && $imageData['caption'] ? $imageData['caption'] : $image->Caption,
+                    'Credit' => array_key_exists('credit', $imageData) && $imageData['credit'] ? $imageData['credit'] : $image->Credit,
+                ]));
+            }
+            return $carry;
+        }, ArrayList::create());
+
+        return ArrayData::create([
+            'Images' => $images,
+        ]);
     }
 }
