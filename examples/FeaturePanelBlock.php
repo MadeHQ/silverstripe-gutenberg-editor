b77c6e68728382e222a9cc5a25780618f8ff893b
hirenpatel <hiren.patel@mademedia.co.uk>
2018-09-18 17:13:16 +0100

    Adding php counterpart

---
+++ examples/FeaturePanelBlock.php
@@ -0,0 +1,27 @@
+<?php
+
+use MadeHQ\Gutenberg\Blocks\BaseBlock;
+
+use SilverStripe\Assets\File;
+use SilverStripe\View\ArrayData;
+use SilverStripe\ORM\DataObject;
+
+class FeaturePanelBlock extends BaseBlock
+{
+    public function render($content, array $attributes = array())
+    {
+        if (!array_key_exists('id', $attributes) || !$attributes['id']) {
+            return '';
+        }
+
+        $panel = DataObject::get_by_id(FeaturePanel::class, $attributes['id']);
+
+        if (!$panel) {
+            return '';
+        }
+
+        $panel->Theme = array_key_exists('theme', $attributes) && $attributes['theme'] ? $attributes['theme'] : 'black';
+
+        return $this->renderWith(sprintf('Blocks/FeaturePanel_%s', $panel->Type), $panel);
+    }
+}
